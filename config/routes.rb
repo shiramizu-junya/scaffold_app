c0a96a86d0563a4f2d7883e443c80dbe17d9b377
shiramizu_junya <jyunya.pg.7231@gmail.com>
2021-02-13 13:08:08 +0900

    Add file

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  # CRUDのルーティングを一括で定義（bin/rails routes）
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
